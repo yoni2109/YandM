4e3d941a196b7a44057c1f77701aeea35cc92b73
MarkRa1 <rabayma@ac.sce.ac.il>
2018-01-16 12:56:37 +0200

    Add Product , fill Dogs Product

--- MVCLabModel1/Models/Products.cs
+++ MVCLabModel1/Models/Products.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace YandM.Models
 {
     public class Products
     {
+        [Required]
         public int price { get; set; }
+        [Required]
+        [Key]
+        public int productId { get; set; }
+        [Required]
         public string product_name { get; set; }
+        [Required]
         public string img_url { get; set; }
+        [Required]
         public string description { get; set; }
+        [Required]
         public string type { get; set; }
+
     }
+
+
 }
